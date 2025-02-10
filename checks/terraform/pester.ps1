fe9cd9f96c777cb2f2e0d265ce3393089a3599d6
Andrew Waite <andrew.waite@ensono.com>
2025-02-10 16:08:57 +0000

    feat: terraform check

---
+++ checks/terraform/pester.ps1
@@ -0,0 +1,59 @@
+param (
+    [Parameter(Mandatory = $true)]
+    [hashtable] $parentConfiguration
+)
+
+BeforeDiscovery {
+    # installing dependencies
+    # to avoid a potential clash with the YamlDotNet libary always load the module 'powershell-yaml' last
+    . ../../powershell/functions/Install-PowerShellModules.ps1
+    Install-PowerShellModules -moduleNames ("powershell-yaml")
+
+    $configurationFilename = $parentConfiguration.configurationFilename
+
+    # loading check configuration
+    if (-not (Test-Path -Path $configurationFilename)) {
+        throw ("Missing configuration file: {0}" -f $configurationFilename)
+    }
+
+    $checkConfiguration = Get-Content -Path $configurationFilename | ConvertFrom-Yaml
+
+    # building the discovery objects
+    $discovery = $checkConfiguration
+} 
+
+Describe $parentConfiguration.displayName -ForEach $discovery {
+
+    Context "Required Version: <_>" -ForEach $_.requiredVersionConstraints {
+        BeforeAll {
+            $testFilePath = "./version.tf"
+        }
+
+        BeforeEach {
+            @"
+            terraform {
+                # https://developer.hashicorp.com/terraform/language/expressions/version-constraints
+                required_version = "$_"
+            }
+"@ | Set-Content -Path $testFilePath -Force
+        }
+
+        It "'terraform init' should return an Exit Code of 0" {            
+            terraform init
+            $LASTEXITCODE | Should -Be 0 
+        }
+
+        AfterEach {
+            Remove-Item -Path $testFilePath -Force
+        }
+
+        AfterAll {
+            Clear-Variable -Name testFilePath
+        }
+    }
+
+    AfterAll {
+        Write-Information -MessageData ("`nInstalled Terraform version: {0}" -f $(terraform --version))
+        Write-Information -MessageData ("`nRunbook: {0}`n" -f $_.runbook)
+    }
+}
